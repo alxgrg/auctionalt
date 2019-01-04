efd4662481eec4d93e66a05738f3b4bec1ddc442
Alex George <alxmgrg@gmail.com>
2019-01-04 12:57:53 -0500

    Fix typo

--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -4,7 +4,7 @@
 			<div class="columns level">
 				<div class="column">
 					<h2 class="title is-blue-title has-text-weight-light">Ready to get started?</h2>
-					<h2 class="subtitle has-text-weight-light">Call us for a free conultation at <strong>(613)-879-1318</strong> or contact us online.</h2>
+					<h2 class="subtitle has-text-weight-light">Call us for a free consultation at <strong>(613)-879-1318</strong> or contact us online.</h2>
 				</div>
 				<div class="column level-item">
 					<a href="/contact" class="button is-outlined is-primary is-large">
