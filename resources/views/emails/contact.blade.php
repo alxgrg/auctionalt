d58e47ea98747a5a704585c2babed32760368ae7
Alex George <alxmgrg@gmail.com>
2019-08-29 14:55:52 -0400

    update

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,19 +1,8 @@
-{{-- Hi,
-
-A user has sent you a message from auctionalternative.ca:
-
-Name: {{ $contact['name'] }}
-
-E-mail: {{ $contact['email'] }}
-
-Message: {{ $contact['message'] }} --}}
-
-
 @component('mail::message')
- <p>A user has sent you a message from auctionalternative.ca: </p>
- <br>
- <p> <strong>Name: </strong> {{ $contact['name'] }} </p>
- <p> <strong>E-mail: </strong> {{ $contact['email'] }} </p>
- <p> <strong>Message: </strong></p>
- <p>{{ $contact['message'] }}</p>
+<p>A user has sent you a message from auctionalternative.ca: </p>
+<br>
+<p> <strong>Name: </strong> {{ $contact['name'] }} </p>
+<p> <strong>E-mail: </strong> {{ $contact['email'] }} </p>
+<p> <strong>Message: </strong></p>
+<p>{{ $contact['message'] }}</p>
 @endcomponent
